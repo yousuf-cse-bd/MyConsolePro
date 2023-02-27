5fc91d17d9b603f86aa1f9a8363b21c323bc71fb
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-02-27 23:53:02 +0600

    Product of tow matrixs using vector and 2D array

---
+++ C++WorkSpace/TophPro/StaticBindingWithInheritance.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file: OperatorOverloading3.cpp
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief: Extraction(>>) & insertion(<<) operator overloading using void in C++
+ * @version: 0.1
+ * @date: 2023-02-24
+ * @since: FriDay; 05:26 PM
+ * @copyright Copyright (c) 2023
+ * @institute: Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char const **argv){
+	system("cls");
+	X x1;
+	cout<<"Enter a value here: ";
+	cin>>x1;
+	cout<<"The entered value: "<<x1;
+	return 0;
+}
